c614aa8533c00bc47f6701953a0e21d35b51d5cf
wfy <201512825@qq.com>
2025-02-24 20:24:52 +0800

    修改包

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define AimPackageName "com.tencent.mobileqq"
+#define AimPackageName "cn.damai"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
